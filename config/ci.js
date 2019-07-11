961ddb89ddbe9654c57fbe6c854f7f43b65cb1db
danyrupes <danyrupes26@gmail.com>
2019-07-11 19:22:54 +0530

    config added

---
+++ config/ci.js
@@ -0,0 +1,9 @@
+module.exports = {
+    googleClientID:
+      '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
+    googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
+    mongoURI: 'mongodb://127.0.0.1:27017/blog_ci',//'mongodb+srv://Dany:xYV5F2jh5jLxa!.@helloworld-yekaf.mongodb.net/blog_dev',
+    cookieKey: '123123123',
+    redisUrl:'redis://127.0.0.1:6379',
+  };
+  
---
+++ config/dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+  googleClientID:
+    '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
+  googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
+  mongoURI: 'mongodb+srv://Dany:xYV5F2jh5jLxa!.@helloworld-yekaf.mongodb.net/blog_dev',
+  cookieKey: '123123123'
+};
